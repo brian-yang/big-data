edc9f246619bed57a78e77d5cfe171440a3a7afb
Kathy Lau <kathy@mit.edu>
2017-03-28 23:22:10 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'dogsrcool'
+
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
