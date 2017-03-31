209d84e0d69497e06f133eac1aa285352e2aa33c
Kathy Lau <imobilep501@gmail.com>
2017-03-31 07:36:18 -0400

    work

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ app.secret_key = 'dogsrcool'
 countries_list = ['China', 'India', 'United States', 'Indonesia', 'Brazil', 'Pakistan', 'Nigeria', 'Bangladesh', 'Russia', 'Japan', 'Mexico', 'Philippines', 'Ethiopia', 'Vietnam','Egypt', 'Iran', 'Germany', 'Turkey', 'Thailand', 'France', 'United Kingdom', 'Italy', 'Burma', 'South Africa']
 @app.route('/')
 def root():
-    #blist = billionaires.get_billionaires()[:10]
+    b_data = billionaires.get_billionaires()
+    b_list = []
     gd_list = []
+    sf_list = []
+    sf_data =  state_fragility.get_scores()
+    for sf in sf_data:
+        if sf['Country'] in countries_list and sf['Country'] not in sf_list:
+            sf_list.append([sf['Country'], sf['Metrics']['State Fragility Index']])
+    for billionaire in b_data:
+        b_list.append([billionaire['name'], billionaire['location']['citizenship']])
     for country in countries_list:
         data = global_development.get_reports_by_country(country)
         if len(data) <=0:
@@ -16,7 +24,7 @@ def root():
         else:
             data = data[0]
         gd_list.append([data['Country'], float(data['Data']['Urban Development']['Urban Population Percent'] )])
-    return render_template("index.html", gd_list=gd_list)
+    return render_template("index.html", gd_list=gd_list, sf_list=sf_list)
 
 if __name__ == '__main__':
     app.debug=True
