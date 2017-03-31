7c4cc792b665f7d196e1c51fa13f455f6f10406d
Brian Yang <brian.yang328@gmail.com>
2017-03-30 23:47:17 -0400

    Work on getting line graph to work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import billionaires, global_development, state_fragility
+from utils import billionaires, global_development, state_fragility
 
 app = Flask(__name__)
 app.secret_key = 'dogsrcool'
@@ -17,6 +17,16 @@ def root():
             data = data[0]
         gd_list.append([data['Country'], float(data['Data']['Urban Development']['Urban Population Percent'] )])
     return render_template("index.html", gd_list=gd_list)
+
+@app.route('/test/')
+def test():
+    # for value in state_fragility.get_scores():
+    #     print
+    #     print value
+        # print value['Metrics']['State Fragility Index']
+        # print value['Metrics']['Country']
+        # print value['Metrics']['Year']
+    return render_template("index2.html")
 
 if __name__ == '__main__':
     app.debug=True
