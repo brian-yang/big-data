a80f394a167faa4156befc5d9047c0be9852d63d
Brian Yang <brian.yang328@gmail.com>
2017-03-31 23:48:40 -0400

    Get both line graphs to display

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, Response
 from utils import billionaires, global_development, state_fragility
+import json
 
 app = Flask(__name__)
 app.secret_key = 'dogsrcool'
@@ -28,18 +29,37 @@ def root():
 
 @app.route('/test/')
 def test():
-    return render_template("index2.html")
+    return render_template("index2.html", country = "United States")
 
-@app.route('/linegraphdata/<country>'):
+# line graph can only read data from this route if it includes a
+# .json extension at the end of the country name
+@app.route('/linegraphdevelopment/<country>.json/')
 def jsonLineGraph(country):
-    for value in state_fragility.get_scores():
-        sf_index = value['Metrics']['State Fragility Index']
-        country = value['Country']
-        year = value['Year']
-        # print "%s\n%s\n%s" % (sf_index, country, year)
-        print "%s,%s" % (year,sf_index)
+    data = []
 
-    return render_template("jsonLineGraph.html", country = country)
+    for value in global_development.get_reports():
+        if country == value['Country']:
+            percent_growth = (round(value['Data']['Urban Development']['Urban Population Percent Growth'] * 10)) / 10
+            year = value['Year']
+            data.append({'index': percent_growth , 'date': year})
+
+    # return render_template("jsonLineGraph.html", data = json.dumps(data))
+    # return jsonify(data)
+    return Response(response = json.dumps(data), status = 200, mimetype='application/json')
+
+@app.route('/linegraphfragility/<country>.json/')
+def jsonLineGraph2(country):
+    data = []
+
+    for value in state_fragility.get_scores():
+        if country == value['Country']:
+            sf_index = value['Metrics']['State Fragility Index']
+            year = value['Year']
+            data.append({'index': sf_index, 'date': year})
+
+    # return render_template("jsonLineGraph.html", data = json.dumps(data))
+    # return jsonify(data)
+    return Response(response = json.dumps(data), status = 200, mimetype='application/json')
 
 if __name__ == '__main__':
     app.debug=True
